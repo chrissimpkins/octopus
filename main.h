b3cade938b5a29efd60d0415c3a0d358efe4d7ca
Chris <git.simpkins@gmail.com>
2013-09-02 00:35:42 -0400

    updated header function declaration

--- main.h
+++ main.h
@@ -14,6 +14,6 @@ inline std::string currentTime();
 inline std::string replaceChar(std::string s, char c1, char c2);
 inline std::string getPostHeader(std::string title, std::string postdate, std::string posttime);
 inline int isDirPresent(std::string& dir);
-inline string pathToPosts();
+inline void pathToPosts(std::string& path);
 
 #endif
