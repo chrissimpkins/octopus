f2881ad8371420c024d4dd68338cab9e025fd0ae
Chris <git.simpkins@gmail.com>
2013-09-22 18:14:21 -0400

    new file check function declaration

--- main.h
+++ main.h
@@ -14,6 +14,7 @@ inline std::string currentTime();
 inline std::string replaceChar(std::string s, char c1, char c2);
 inline std::string getPostHeader(std::string title, std::string postdate, std::string posttime);
 inline int isDirPresent(std::string& dir);
+inline int isFilePresent(std::string& file);
 inline void pathToPosts(std::string& path_to_posts);
 inline void pathToPublic(std::string& path_to_public);
 inline void pathToDir(std::string& directory_string, std::string& path_to_dir);
